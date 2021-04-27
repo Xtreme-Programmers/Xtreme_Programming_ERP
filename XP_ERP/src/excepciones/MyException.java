12a1e64d69776ef9110fd4ff74621a7b8f745958
Ryan0mor <71499530+Ryan0mor@users.noreply.github.com>
2021-04-27 18:08:51 +0200

    Primer Push

---
+++ XP_ERP/src/excepciones/MyException.java
@@ -0,0 +1,16 @@
+/*
+    Centaliza las excepciones
+ */
+package excepciones;
+
+
+/**
+ *
+ * @author Ryan
+ */
+public class MyException extends Exception{
+    
+    public MyException (String mensaje){
+        super(mensaje);
+    }
+}
