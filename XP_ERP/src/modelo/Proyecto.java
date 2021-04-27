c2d69e08f115e2d679e81088000157fe75f3bc21
Ryan0mor <71499530+Ryan0mor@users.noreply.github.com>
2021-04-27 19:34:49 +0200

    Cargada clase fecha y comienzo de la clase proyecto

---
+++ XP_ERP/src/modelo/Proyecto.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author ryano
+ */
+public class Proyecto {
+    private int idProyecto;
+    private String nombreProyecto;
+    private String descripción;
+    private HashSet<Story> listaStories;
+    private 
+}
