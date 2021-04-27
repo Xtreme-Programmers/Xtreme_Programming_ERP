e63f81d99a42707700fcf846174d755774248a12
Ryan0mor <71499530+Ryan0mor@users.noreply.github.com>
2021-04-27 18:21:07 +0200

    Clases que ha generado el equipo poparicio

---
+++ XP_ERP/src/gestoras/GestoraEquipo.java
@@ -0,0 +1,49 @@
+/*
+ *Clase Gestora
+ */
+package gestora;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import modelo.MiembroEquipo;
+
+/**
+ *
+ * @author danie
+ */
+public class GestoraEquipo {
+
+    private static HashSet<MiembroEquipo> listadoMiembros = new HashSet<>();
+
+    public static boolean addMiembro(MiembroEquipo mE) {
+        return listadoMiembros.add(mE);
+    }
+
+    /**
+     *
+     * @param codigo
+     * @return True si se puede eliminar del listado de miembros
+     */
+    public static boolean borrarMiembroCodigo(String codigo) {
+        Iterator it = listadoMiembros.iterator();
+        while (it.hasNext()) {
+            MiembroEquipo next = (MiembroEquipo) it.next();
+            if (next.getIdMiembro().equalsIgnoreCase(codigo)) {
+                return listadoMiembros.remove(next);
+            }
+        }
+        return false;
+    }
+
+    public static boolean loguearMiembro(String nick, String password) {
+        Iterator it = listadoMiembros.iterator();
+        while (it.hasNext()) {
+            MiembroEquipo next = (MiembroEquipo) it.next();
+            if (next.getIdMiembro().equalsIgnoreCase(nick) && next.getPass().equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ XP_ERP/src/modelo/MiembroEquipo.java
@@ -0,0 +1,112 @@
+package modelo;
+
+import MyException.MyException;
+import java.util.HashSet;
+import java.util.Objects;
+
+public class MiembroEquipo {
+
+    public static final String[] ROLES = {"Programador", "Tester", "Tracker", "Trainner", "Manager", "Cliente"};
+
+    private String idMiembro;
+    private String pass;
+    private static HashSet<String> rolesMiembros = new HashSet<>();
+
+    // Getters & Setters
+    public String getIdMiembro() {
+        return idMiembro;
+    }
+
+    public void setIdMiembro(String idMiembro) throws MyException {
+        if (idMiembro == null) {
+            throw new MyException("El id del miembro no puede ser nulo");
+        }
+        if (idMiembro.equals("")) {
+            throw new MyException("El id del miembro no puede ser vacio");
+        }
+        this.idMiembro = idMiembro;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) throws MyException {
+        if (pass == null) {
+            throw new MyException("El password no puede ser nulo");
+        }
+        if (pass.equals("")) {
+            throw new MyException("EL password no puede estar vacio");
+        }
+        this.pass = pass;
+    }
+
+    //Gestora
+    public static boolean addRol(String rol) {
+        return rolesMiembros.add(rol);
+    }
+
+    public static boolean removeRol(String rol) {
+        return rolesMiembros.remove(rol);
+    }
+
+    /**
+     *
+     * @param entradaPorTeclado
+     * @return False si no se puede añadir el rol
+     * @throws Exception
+     */
+    public static boolean setRol(String entradaPorTeclado) throws Exception {
+        String rol = buscarRolNombre(entradaPorTeclado);
+
+        if (!rol.equals("")) {
+            return rolesMiembros.add(entradaPorTeclado);
+        }
+        return false;
+    }
+
+    public static String buscarRolNombre(String nombre) {
+        for (int i = 0; i < ROLES.length; i++) {
+            if (ROLES[i].equalsIgnoreCase(nombre)) {
+                return ROLES[i];
+            }
+        }
+        return "";
+    }
+
+    public boolean eliminarRol(String codigo) {
+        for (String s : ROLES) {
+            if (s.equalsIgnoreCase(codigo)) {
+                return rolesMiembros.remove(s);
+            }
+        }
+        return false;
+    }
+
+    // Equals y HashCode
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 37 * hash + Objects.hashCode(this.idMiembro);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MiembroEquipo other = (MiembroEquipo) obj;
+        if (!Objects.equals(this.idMiembro, other.idMiembro)) {
+            return false;
+        }
+        return true;
+    }
+
+}
