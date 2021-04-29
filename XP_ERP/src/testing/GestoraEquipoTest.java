5d4e02977e65701ceaa55efca1971c457b0db9ec
SAM19746 <83002726+SAM19746@users.noreply.github.com>
2021-04-29 09:08:05 +0200

    testing

---
+++ XP_ERP/src/testing/GestoraEquipoTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testing;
+
+import gestora.GestoraEquipo;
+import modelo.MiembroEquipo;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author LopezSamuel
+ */
+public class GestoraEquipoTest {
+    
+    public GestoraEquipoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addMiembro method, of class GestoraEquipo.
+     */
+    @Test
+    public void testAddMiembro() {
+        System.out.println("addMiembro");
+        MiembroEquipo mE = null;
+        boolean expResult = false;
+        boolean result = GestoraEquipo.addMiembro(mE);
+        assertEquals(expResult, result);
+        fail("¡¡¡El intento de añadir un miembro falló!!!");
+    }
+
+    /**
+     * Test of borrarMiembroCodigo method, of class GestoraEquipo.
+     */
+    @Test
+    public void testBorrarMiembroCodigo() {
+        System.out.println("borrarMiembroCodigo");
+        String codigo = "";
+        boolean expResult = false;
+        boolean result = GestoraEquipo.borrarMiembroCodigo(codigo);
+        assertEquals(expResult, result);
+        fail("¡¡¡El intento de borrar un miembro falló!!!");
+    }
+
+    /**
+     * Test of loguearMiembro method, of class GestoraEquipo.
+     */
+    @Test
+    public void testLoguearMiembro() {
+        System.out.println("loguearMiembro");
+        String nick = "";
+        String password = "";
+        boolean expResult = false;
+        boolean result = GestoraEquipo.loguearMiembro(nick, password);
+        assertEquals(expResult, result);
+        fail("¡¡¡Los datos establecidos para añadir nuevo miembro no han sido aceptados!!!");
+    }
+    
+}
---
+++ XP_ERP/src/testing/MiembroEquipoTest.java
@@ -0,0 +1,189 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testing;
+
+import modelo.MiembroEquipo;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author LopezSamuel
+ */
+public class MiembroEquipoTest {
+    
+    public MiembroEquipoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getIdMiembro method, of class MiembroEquipo.
+     */
+    @Test
+    public void testGetIdMiembro() {
+        System.out.println("getIdMiembro");
+        MiembroEquipo instance = new MiembroEquipo();
+        String expResult = "";
+        String result = instance.getIdMiembro();
+        assertEquals(expResult, result);
+        fail("¡¡¡No se obtuvieron datos del tipo deseado!!!");
+    }
+
+    /**
+     * Test of setIdMiembro method, of class MiembroEquipo.
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testSetIdMiembro() throws Exception {
+        System.out.println("setIdMiembro");
+        String idMiembro = "";
+        MiembroEquipo instance = new MiembroEquipo();
+        instance.setIdMiembro(idMiembro);
+        fail("¡¡¡Los datos que se querían establecer no se añadieron!!!");
+    }
+
+    /**
+     * Test of getPass method, of class MiembroEquipo.
+     */
+    @Test
+    public void testGetPass() {
+        System.out.println("getPass");
+        MiembroEquipo instance = new MiembroEquipo();
+        String expResult = "";
+        String result = instance.getPass();
+        assertEquals(expResult, result);
+        fail("¡¡¡No se obtuvieron datos del tipo deseado!!!");
+    }
+
+    /**
+     * Test of setPass method, of class MiembroEquipo.
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testSetPass() throws Exception {
+        System.out.println("setPass");
+        String pass = "";
+        MiembroEquipo instance = new MiembroEquipo();
+        instance.setPass(pass);
+        fail("¡¡¡Los datos que se querían establecer no se añadieron!!!");
+    }
+
+    /**
+     * Test of addRol method, of class MiembroEquipo.
+     */
+    @Test
+    public void testAddRol() {
+        System.out.println("addRol");
+        String rol = "";
+        boolean expResult = false;
+        boolean result = MiembroEquipo.addRol(rol);
+        assertEquals(expResult, result);
+        fail("¡¡¡No se encontró rol a añadir!!!");
+    }
+
+    /**
+     * Test of removeRol method, of class MiembroEquipo.
+     */
+    @Test
+    public void testRemoveRol() {
+        System.out.println("removeRol");
+        String rol = "";
+        boolean expResult = false;
+        boolean result = MiembroEquipo.removeRol(rol);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("¡¡¡No se encontró rol que eliminar!!!");
+    }
+
+    /**
+     * Test of setRol method, of class MiembroEquipo.
+     * @throws java.lang.Exception
+     */
+    @Test
+    public void testSetRol() throws Exception {
+        System.out.println("setRol");
+        String entradaPorTeclado = "";
+        boolean expResult = false;
+        boolean result = MiembroEquipo.setRol(entradaPorTeclado);
+        assertEquals(expResult, result);
+        fail("¡¡¡Los datos que se querían establecer no se añadieron!!!");
+    }
+
+    /**
+     * Test of buscarRolNombre method, of class MiembroEquipo.
+     */
+    @Test
+    public void testBuscarRolNombre() {
+        System.out.println("buscarRolNombre");
+        String nombre = "";
+        String expResult = "";
+        String result = MiembroEquipo.buscarRolNombre(nombre);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("¡¡¡Los datos a buscar no son cadena!!!");
+    }
+
+    /**
+     * Test of eliminarRol method, of class MiembroEquipo.
+     */
+    @Test
+    public void testEliminarRol() {
+        System.out.println("eliminarRol");
+        String codigo = "";
+        MiembroEquipo instance = new MiembroEquipo();
+        boolean expResult = false;
+        boolean result = instance.eliminarRol(codigo);
+        assertEquals(expResult, result);
+        fail("¡¡¡No se pudo eliminar el rol solicitado!!!");
+    }
+
+    /**
+     * Test of hashCode method, of class MiembroEquipo.
+     */
+    @Test
+    public void testHashCode() {
+        System.out.println("hashCode");
+        MiembroEquipo instance = new MiembroEquipo();
+        int expResult = 0;
+        int result = instance.hashCode();
+        assertEquals(expResult, result);
+        fail("¡¡¡Hash no generado!!!");
+    }
+
+    /**
+     * Test of equals method, of class MiembroEquipo.
+     */
+    @Test
+    public void testEquals() {
+        System.out.println("equals");
+        Object obj = null;
+        MiembroEquipo instance = new MiembroEquipo();
+        boolean expResult = false;
+        boolean result = instance.equals(obj);
+        assertEquals(expResult, result);
+        fail("¡¡¡No se encontró ningún elemento coincidente!!!");
+    }
+    
+}
