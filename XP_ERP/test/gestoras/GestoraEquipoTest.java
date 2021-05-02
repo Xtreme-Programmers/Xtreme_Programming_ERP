ecd8be7e54307588f01d6db7d98a74da79092507
TelloBer <83299271+TelloBer@users.noreply.github.com>
2021-05-02 12:50:37 +0200

    updateGestorasJUnit

--- XP_ERP/test/gestoras/GestoraEquipoTest.java
+++ XP_ERP/test/gestoras/GestoraEquipoTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package testing;
+package gestoras;
 
 import excepciones.MyException;
 import gestoras.GestoraEquipo;
