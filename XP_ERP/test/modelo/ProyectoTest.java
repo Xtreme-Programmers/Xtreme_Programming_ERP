36d988573b349f5a80ba1fc133a5fc76a7433eee
TelloBer <83299271+TelloBer@users.noreply.github.com>
2021-05-02 19:00:19 +0200

    Update ProyectoTest

--- XP_ERP/test/modelo/ProyectoTest.java
+++ XP_ERP/test/modelo/ProyectoTest.java
@@ -50,37 +50,110 @@ public class ProyectoTest {
      * Test of setNombreProyecto method, of class Proyecto.
      */
     @Test
-    public void testSetNombreProyecto() throws Exception {
+    public void testSetNombreProyectoSi() throws Exception {
         System.out.println("Si setNombreProyecto");
         String nombreProyecto = "aaaa";
         instanceP.setNombreProyecto(nombreProyecto);
         assertEquals(nombreProyecto, instanceP.getNombreProyecto());
+    }
+    
+    @Test
+    public void testSetNombreProyectoNo1() throws Exception {
+        System.out.println("No setNombreProyecto nombre nulo");
+        String nombreProyecto = null;
+        try{
+            instanceP.setNombreProyecto(nombreProyecto);
+        }catch(MyException ex){
+            System.out.println("--NoSetNombre-- " + ex.getMessage());
+        } 
+    }
+    
+    @Test
+    public void testSetNombreProyectoNo2() throws Exception {
+        System.out.println("No setNombreProyecto nombre vacío");
+        String nombreProyecto = "";
+        try{
+            instanceP.setNombreProyecto(nombreProyecto);
+        }catch(MyException ex){
+            System.out.println("--NoSetNombre-- " + ex.getMessage());
+        } 
     }
 
     /**
      * Test of setDescripcion method, of class Proyecto.
      */
     @Test
-    public void testSetDescripcion() throws Exception {
-        System.out.println("setDescripcion");
+    public void testSetDescripcionSi() throws Exception {
+        System.out.println("Si setDescripcion");
+        String descripcion = "aaaa";
+        instanceP.setDescripcion(descripcion);
+        assertEquals(descripcion, instanceP.getDescripcion());
+    }
+    
+    @Test
+    public void testSetDescripcionNo1() throws Exception {
+        System.out.println("No setDescripcion descripción nula");
+        String descripcion = null;
+        try{
+            instanceP.setDescripcion(descripcion);
+        }catch(MyException ex){
+            System.out.println("--NoSetNombre-- " + ex.getMessage());
+        } 
+    }
+    
+    @Test
+    public void testSetDescripcionNo2() throws Exception {
+        System.out.println("No setDescripcion descripción vacía");
         String descripcion = "";
-        Proyecto instance = null;
-        instance.setDescripcion(descripcion);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        try{
+            instanceP.setDescripcion(descripcion);
+        }catch(MyException ex){
+            System.out.println("--NoSetNombre-- " + ex.getMessage());
+        } 
     }
 
     /**
      * Test of setEstadoProyecto method, of class Proyecto.
      */
     @Test
-    public void testSetEstadoProyecto() throws Exception {
-        System.out.println("setEstadoProyecto");
+    public void testSetEstadoProyectoSi() throws Exception {
+        System.out.println("Si setEstadoProyecto");
+        String estadoProyecto = "enCurso";
+        instanceP.setEstadoProyecto(estadoProyecto);
+        assertEquals(estadoProyecto, instanceP.getEstadoProyecto());
+    }
+    
+    @Test
+    public void testSetEstadoProyectoNo1() throws Exception {
+        System.out.println("No setEstadoProyecto estado nulo");
+        String estadoProyecto = null;
+        try{
+            instanceP.setEstadoProyecto(estadoProyecto);
+        }catch(MyException ex){
+            System.out.println("--NoSetEstadoProyecto-- " + ex.getMessage());
+        }
+    }
+    
+    @Test
+    public void testSetEstadoProyectoNo2() throws Exception {
+        System.out.println("No setEstadoProyecto estado vacío");
         String estadoProyecto = "";
-        Proyecto instance = null;
-        instance.setEstadoProyecto(estadoProyecto);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        try{
+            instanceP.setEstadoProyecto(estadoProyecto);
+        }catch(MyException ex){
+            System.out.println("--NoSetEstadoProyecto-- " + ex.getMessage());
+        }
+    }
+    
+    @Test
+    public void testSetEstadoProyectoNo3() throws Exception {
+        System.out.println("No setEstadoProyecto estado no existente");
+        String estadoProyecto = "asdfgh";
+        try{
+            instanceP.setEstadoProyecto(estadoProyecto);
+        }catch(MyException ex){
+            System.out.println("--NoSetEstadoProyecto-- " + ex.getMessage());
+        }
     }
 
     /**
@@ -88,13 +161,10 @@ public class ProyectoTest {
      */
     @Test
     public void testGetIdProyecto() {
-        System.out.println("getIdProyecto");
-        Proyecto instance = null;
+        System.out.println("Si getIdProyecto");
         int expResult = 0;
-        int result = instance.getIdProyecto();
+        int result = instanceP.getIdProyecto();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -102,13 +172,10 @@ public class ProyectoTest {
      */
     @Test
     public void testGetNombreProyecto() {
-        System.out.println("getNombreProyecto");
-        Proyecto instance = null;
-        String expResult = "";
-        String result = instance.getNombreProyecto();
+        System.out.println("Si getNombreProyecto");
+        String expResult = "aaaa";
+        String result = instanceP.getNombreProyecto();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -116,13 +183,10 @@ public class ProyectoTest {
      */
     @Test
     public void testGetDescripcion() {
-        System.out.println("getDescripcion");
-        Proyecto instance = null;
-        String expResult = "";
-        String result = instance.getDescripcion();
+        System.out.println("Si getDescripcion");
+        String expResult = "lalala";
+        String result = instanceP.getDescripcion();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
     /**
@@ -130,41 +194,9 @@ public class ProyectoTest {
      */
     @Test
     public void testGetEstadoProyecto() {
-        System.out.println("getEstadoProyecto");
-        Proyecto instance = null;
-        String expResult = "";
-        String result = instance.getEstadoProyecto();
+        System.out.println("Si getEstadoProyecto");
+        String expResult = "enCurso";
+        String result = instanceP.getEstadoProyecto();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-
-    /**
-     * Test of getListaStories method, of class Proyecto.
-     */
-    @Test
-    public void testGetListaStories() {
-        System.out.println("getListaStories");
-        Proyecto instance = null;
-        HashSet<Story> expResult = null;
-        HashSet<Story> result = instance.getListaStories();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of toString method, of class Proyecto.
-     */
-    @Test
-    public void testToString() {
-        System.out.println("toString");
-        Proyecto instance = null;
-        String expResult = "";
-        String result = instance.toString();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
 }
